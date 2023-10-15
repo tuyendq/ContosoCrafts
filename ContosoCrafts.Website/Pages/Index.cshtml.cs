4e0a380b08d2025ffab65c62a77aee1f16c06f94
Tuyen Dang <tuyendq@gmail.com>
2023-10-15 23:09:09 +0700

    Initial commit

---
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.Website.Services;
+using ContosoCrafts.Website.Models;
+
+namespace ContosoCrafts.Website.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger,
+     JsonFileProductService productService)
+    {
+        _logger = logger;
+            ProductService = productService;
+
+    }
+
+    public JsonFileProductService ProductService { get; }
+public IEnumerable<Product> Products { get; private set; }
+
+    public void OnGet()
+    {
+    Products = ProductService.GetProducts();
+    }
+}
---
+++ ContosoCrafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,33 @@
+
+using System.Text.Json;
+using ContosoCrafts.Website.Models;
+
+namespace ContosoCrafts.Website.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using(var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }        
+    }
+}
